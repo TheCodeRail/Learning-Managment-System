abac6a8a706b4e086c81c0f5042071609b33578a
Akshatjain <akshat.2024it1031@kiet.edu>
2024-07-15 12:00:45 +0530

    added one more point in subs

--- client/src/webcomponents/Subscription.jsx
+++ client/src/webcomponents/Subscription.jsx
@@ -192,6 +192,24 @@ const Subscription = () => {
                       </span>
                     </span>
                   </li>
+                  <li class="flex items-center space-x-3">
+                    <svg
+                      class="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                        clip-rule="evenodd"
+                      ></path>
+                    </svg>
+                    <span>
+                      Mock Interviews:{" "}
+                      <span class="font-semibold">Free 2 Mock Interviews</span>
+                    </span>
+                  </li>
                 </ul>
                 <Link to="/enroll">
                   <Button>Enroll Now</Button>
